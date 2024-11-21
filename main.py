3ee0b238928baa967bfa1c516f97c0115c2c8f1c
scribebo <adalene.leigh@gmail.com>
2024-11-21 18:15:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,31 @@ while True:
 		text = text + '\n' + entered
 
 character_count = len(text)
-print('Character count (includes spaces and newlines):', character_count)
 
 text_corrected = text.replace('—', ' ') #because you are NOT supposed to put spaces around your em dashes!!! i WILL make the assumption that people know how to use punctuation correctly!!
-word_count = len(text_corrected.split())
-print('Word count:', word_count)
-
 text_corrected = text_corrected.replace('!', '.')
 text_corrected = text_corrected.replace('?', '.')
-sentences = text_corrected.split('. ')
-words_per_sentence = word_count/len(sentences)
+text_corrected = text_corrected.replace('. . .', '')
+text_corrected = text_corrected.replace(' .', '.')
 
+words = text_corrected.split()
+sentences = text_corrected.split('. ')
+
+longest_sentence = ''
+shortest_sentence = ''
+max_words = 0
+min_words = float('inf')
+for sentence in sentences:
+	if len(sentence.split()) > max_words:
+		longest_sentence = sentence
+		max_words = len(sentence.split())
+	if len(sentence.split()) < min_words:
+		shortest_sentence = sentence
+		min_words = len(sentence.split())
+
+word_count = len(words)
+sentence_count = len(sentences)
+words_per_sentence = word_count/sentence_count
+
+print('Character count (includes spaces and newlines):', character_count)
+print('Word count:', word_count)
