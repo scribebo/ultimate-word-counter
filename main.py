6add57e292766b671bdf45407f97735080a26bf6
scribebo <adalene.leigh@gmail.com>
2024-11-21 16:28:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 text = ''
 while True:
-	
+	entered = input('enter or paste your text, one paragraph at a time please or it won't work! when you\'re done, just enter a single backslash!\n')
+	if entered == '\':
+		break
+	else:
+		text = text + '\n' + entered
+
+text_corrected_for_words = text.replace('—', ' ')
