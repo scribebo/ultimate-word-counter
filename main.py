2087579db7d476832241f8d2e88ce7a80bffbe68
scribebo <adalene.leigh@gmail.com>
2024-11-21 17:31:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 text = ''
+
+print('enter or paste your text, one paragraph at a time please or it won\'t work! when you\'re done, just enter a single backslash! also, values will probably (?) not be accurate if you put spaces around your em dashes, so uh. yeah.')
 while True:
-	entered = input('enter or paste your text, one paragraph at a time please or it won't work! when you\'re done, just enter a single backslash! also, values will probably (?) not be accurate if you put spaces around your em dashes, so uh. yeah.\n')
-	if entered == '\':
+	entered = input('enter text below:\n')
+	if entered == '\\':
 		break
 	else:
 		text = text + '\n' + entered
@@ -10,7 +12,7 @@ character_count = len(text)
 print('Character count (includes spaces and newlines):', character_count)
 
 text_corrected = text.replace('—', ' ') #because you are NOT supposed to put spaces around your em dashes!!! i WILL make the assumption that people know how to use punctuation correctly!!
-word_count = len(text_corrected_for_words)
+word_count = len(text_corrected.split())
 print('Word count:', word_count)
 
 text_corrected = text_corrected.replace('!', '.')
